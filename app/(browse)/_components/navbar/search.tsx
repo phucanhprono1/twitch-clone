86e601a747ba4935e6c7d460f7b6a9b3c0f3e7af
phucanhprono1 <90205324+phucanhprono1@users.noreply.github.com>
2024-01-07 23:22:25 +0700

    search page

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -1,53 +1,59 @@
 "use client";
 
 import qs from "query-string";
+import { useState } from "react";
+import { SearchIcon, X } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { SearchIcon,X } from "lucide-react";
-import { Button } from "@/components/ui/button";
+
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 export const Search = () => {
     const router = useRouter();
     const [value, setValue] = useState("");
-    const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        if(!value) return;
+
+        if (!value) return;
+
         const url = qs.stringifyUrl({
             url: "/search",
             query: { term: value },
         }, { skipEmptyString: true });
+
         router.push(url);
     };
+
     const onClear = () => {
         setValue("");
-        
     };
+
     return (
-        <form 
+        <form
             onSubmit={onSubmit}
-            className="relative w-full  lg:w-[400px] flex items-center"
-            >
-                <Input
-                    value={value}
-                    onChange={(e) => setValue(e.target.value)}
-                    type="text"
-                    placeholder="Search"
-                    className="rounded-r-none focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:ring-offset-transparent"
+            className="relative w-full lg:w-[400px] flex items-center"
+        >
+            <Input
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                placeholder="Search"
+                className="rounded-r-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
+            />
+            {value && (
+                <X
+                    className="absolute top-2.5 right-14 h-5 w-5 text-muted-foreground cursor-pointer hover:opacity-75 transition"
+                    onClick={onClear}
                 />
-                {value && (
-                    <X
-                        className="absolute top-2.5 right-14 w-5 h-5 text-muted-foreground cursor-pointer hover:opacity-75"
-                        onClick={onClear}
-                        />
-                    )}
-                    <Button
-                        type="submit"
-                        size="sm"
-                        variant="secondary"
-                        className="rounded-l-none"
-                        >
-                            <SearchIcon className="w-5 h-5 text-muted-foreground" />
-                        </Button>
-            </form>
+            )}
+            <Button
+                type="submit"
+                size="sm"
+                variant="secondary"
+                className="rounded-l-none"
+            >
+                <SearchIcon className="h-5 w-5 text-muted-foreground" />
+            </Button>
+        </form>
     );
 };
