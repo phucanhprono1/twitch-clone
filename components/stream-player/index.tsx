37dfa225f1fc6609c7f36897f44f5ae2a5662678
phucanhprono1 <90205324+phucanhprono1@users.noreply.github.com>
2024-01-07 22:22:08 +0700

    about card(bio)

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -5,11 +5,12 @@ import { useViewerToken } from "@/hooks/use-viewer-token";
 import { LiveKitRoom } from "@livekit/components-react";
 import { Video, VideoSkeleton } from "./video";
 import { useChatSidebar } from "@/store/use-chat-sidebar";
-import { Chat } from "./chat";
+import { Chat, ChatSkeleton } from "./chat";
 import { ChatToggle } from "./chat-toggle";
 import { cn } from "@/lib/utils";
-import { Header } from "./header";
+import { Header, HeaderSkeleton } from "./header";
 import { InfoCard } from "./info-card";
+import { AboutCard } from "./about-card";
 
 type CustomUser = {
     id: string;
@@ -88,6 +89,13 @@ export const StreamPlayer = async ({
                         name={stream.name}
                         thumbnailUrl={stream.thumbnailUrl}
                     />
+                    <AboutCard
+                        hostName={user.username}
+                        hostIdentity={user.id}
+                        viewerIdentity={identity}
+                        bio={user.bio}
+                        followedByCount={user._count.followedBy}
+                    />
                 </div>
                 <div
                     className={cn(
@@ -113,14 +121,14 @@ export const StreamPlayer = async ({
 };
 export const StreamPlayerSkeleton = () => {
     return (
-        <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
-            <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
-                <VideoSkeleton />
-
-            </div>
-            <div className="col-span-1 bg-background">
-
-            </div>
+      <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
+        <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+          <VideoSkeleton />
+          <HeaderSkeleton />
         </div>
+        <div className="col-span-1 bg-background">
+          <ChatSkeleton />
+        </div>
+      </div>
     )
-}
+  }
